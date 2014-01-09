6f607340fb96e5e835f68a64c01bed5403b99438
Henri Roussez <hroussez@skyward.dyn.toronto.transgaming.com>
2014-01-09 12:27:08 -0500

    init: print hostname

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<head>
+    <title>hello world<title>
+</head>
+<body>
+    <h1>hello world!</h1>
+        <p>info:
+        <ul>
+            <li>hostname: <?php print eval("hostname") ?></li>
+        </ul>
+    </p>
+</body>
